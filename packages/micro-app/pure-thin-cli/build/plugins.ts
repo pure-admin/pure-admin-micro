e1a949d3de91c2f6a44b3d3fdf97235e9e9c5d7a
lrl <742798240@qq.com>
2023-02-04 13:27:03 +0800

    fix: 添加micro-app自定义元素配置

--- packages/micro-app/pure-thin-cli/build/plugins.ts
+++ packages/micro-app/pure-thin-cli/build/plugins.ts
@@ -20,7 +20,13 @@ export function getPluginsList(
   const prodMock = true;
   const lifecycle = process.env.npm_lifecycle_event;
   return [
-    vue(),
+    vue({
+      template: {
+        compilerOptions: {
+          isCustomElement: tag => /^micro-app/.test(tag)
+        }
+      }
+    }),
     // jsx、tsx语法支持
     vueJsx(),
     VITE_CDN ? cdn : null,
